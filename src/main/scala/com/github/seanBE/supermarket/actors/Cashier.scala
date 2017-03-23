c5af179e1b6e494379544c018a1770a2b4b748a9
seanBE <SeanBE@users.noreply.github.com>
2017-03-23 18:37:16 +0100

    put more logging in cashier receive statement

--- src/main/scala/com/github/seanBE/supermarket/actors/Cashier.scala
+++ src/main/scala/com/github/seanBE/supermarket/actors/Cashier.scala
@@ -4,8 +4,12 @@ import akka.actor.{Actor, ActorLogging, Props}
 
 class Cashier extends Actor with ActorLogging {
   override def receive = {
-    case "Hello" => sender ! "Hello, I am the cashier!"
-    case "Checkout" => sender ! Cashier.Receipt(Nil)
+    case "Hello" =>
+      log.info("Cashier saying hello")
+      sender ! "Hello, I am the cashier!"
+    case "Checkout" =>
+      log.info("Checking out.")
+      sender ! Cashier.Receipt(Nil)
     case _ =>
       log.info("Unknown Request")
       sender ! "Huh?"
